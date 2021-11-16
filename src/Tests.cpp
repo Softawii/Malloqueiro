7df4977f68543f0e218b2a32403bc1f2769a9826
FerroEduardo <47820549+FerroEduardo@users.noreply.github.com>
2021-11-16 19:34:15 -0300

    Adiciona Teste 2: Alocação de matriz nXm

--- src/Tests.cpp
+++ src/Tests.cpp
@@ -33,7 +33,41 @@ void test_1() {
     std::cout << "Teste 1: encerrado sem falhas" << std::endl;
 }
 
+/*
+Teste que aloca uma matriz n X m e verifica se os valores não foram alterados no processo
+*/
+void test_2() {
+    std::cout << "Teste 2: iniciando" << std::endl;
+    size_t n = 5;
+    size_t m = 5;
+    int **inteiros = (int **) Malloqueiro::malloc(sizeof(int*) * n);
+    for (size_t i = 0; i < n; i++) {
+        inteiros[i] = (int *) Malloqueiro::malloc(sizeof(int) * m);
+    }
+
+    for (size_t i = 0; i < n; i++) {
+        for (size_t j = 0; j < m; j++) {
+            inteiros[i][j] = 99;
+        }
+    }
+
+    for (size_t i = 0; i < n; i++) {
+        for (size_t j = 0; j < m; j++) {
+            assert(inteiros[i][j] == 99);
+        }
+    }
+    
+    assert(MalloqueiroGerency::memoryState() == (sizeof(int*) * n) + (sizeof(int) * n * m));
+    for (size_t i = 0; i < n; i++) {
+        assert(Malloqueiro::free(inteiros[i]) == true);
+    }
+    assert(Malloqueiro::free(inteiros) == true);
+    assert(MalloqueiroGerency::memoryState() == -1llu);
+    std::cout << "Teste 2: encerrado sem falhas" << std::endl;
+}
+
 int main(int argc, char const *argv[]) {
     test_1();
+    test_2();
     return 0;
 }
