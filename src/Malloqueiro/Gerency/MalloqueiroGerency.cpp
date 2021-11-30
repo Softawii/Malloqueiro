7afb2474d2c254288ceab58a08422c2a36477f66
FerroEduardo <47820549+FerroEduardo@users.noreply.github.com>
2021-11-30 10:13:43 -0300

    New gerency functions

--- src/Malloqueiro/Gerency/MalloqueiroGerency.cpp
+++ src/Malloqueiro/Gerency/MalloqueiroGerency.cpp
@@ -34,4 +34,34 @@ namespace MalloqueiroGerency {
         return totalAlocado;
     };
 
+    /**
+     * @brief Exibe o estado da memória alocada atual
+     * 
+     * @return Retorna o total de memória alocada
+     */
+    unsigned long long freeBlocks(){
+        MalloqueiroLowLevel::t_block block = (MalloqueiroLowLevel::t_block) MalloqueiroLowLevel::base;
+        if (block == NULL) {
+        #ifndef TEST
+            std::cout << "Nenhum bloco foi alocado" << std::endl;
+        #endif
+            return -1;
+        }
+        
+    #ifndef TEST
+        std::cout << "Blocos livres:" << std::endl;
+    #endif
+        unsigned long long totalBlocosLivres = 0;
+        for (size_t i = 0; block != NULL; i++, block = block->next) {
+        #ifndef TEST
+            std::cout << "Bloco " << i << ": está livre?" << (block->free ? "Sim":"Não") << std::endl;
+        #endif
+            totalBlocosLivres += block->size;
+        }
+    #ifndef TEST
+        std::cout << "Total de blocos livres: " << totalBlocosLivres << " bytes" << std::endl;
+    #endif
+        return totalBlocosLivres;
+    };
+
 }
