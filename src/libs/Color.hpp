076ee8774a8c163392be248ac701f065660cc1d5
FerroEduardo <47820549+FerroEduardo@users.noreply.github.com>
2021-11-30 11:06:14 -0300

    Novas bibliotecas

---
+++ src/libs/Color.hpp
@@ -0,0 +1,51 @@
+#define COLOR_RESET "\033[0m"       //Text Reset
+
+//Regular Colors
+#define BLACK "\033[0;30m"        // BLACK
+#define RED "\033[0;31m"          // RED
+#define GREEN "\033[0;32m"        // GREEN
+#define YELLOW "\033[0;33m"       // YELLOW
+#define BLUE "\033[0;34m"         // BLUE
+#define PURPLE "\033[0;35m"       // PURPLE
+#define CYAN "\033[0;36m"         // CYAN
+#define WHITE "\033[0;37m"        // WHITE
+
+//Bold
+#define BBLACK "\033[1;30m"       // BLACK
+#define BRED "\033[1;31m"         // RED
+#define BGREEN "\033[1;32m"       // GREEN
+#define BYELLOW "\033[1;33m"      // YELLOW
+#define BBLUE "\033[1;34m"        // BLUE
+#define BPURPLE "\033[1;35m"      // PURPLE
+#define BCYAN "\033[1;36m"        // CYAN
+#define BWHITE "\033[1;37m"       // WHITE
+
+// Underline
+#define UBLACK "\033[4;30m"       // BLACK
+#define URED "\033[4;31m"         // RED
+#define UGREEN "\033[4;32m"       // GREEN
+#define UYELLOW "\033[4;33m"      // YELLOW
+#define UBLUE "\033[4;34m"        // BLUE
+#define UPURPLE "\033[4;35m"      // PURPLE
+#define UCYAN "\033[4;36m"        // CYAN
+#define UWHITE "\033[4;37m"       // WHITE
+
+// High Intensty
+#define IBLACK "\033[0;90m"       // BLACK
+#define IRED "\033[0;91m"         // RED
+#define IGREEN "\033[0;92m"       // GREEN
+#define IYELLOW "\033[0;93m"      // YELLOW
+#define IBLUE "\033[0;94m"        // BLUE
+#define IPURPLE "\033[0;95m"      // PURPLE
+#define ICYAN "\033[0;96m"        // CYAN
+#define IWHITE "\033[0;97m"       // WHITE
+
+// Bold High Intensty
+#define BIBLACK "\033[1;90m"      // BLACK
+#define BIRED "\033[1;91m"        // RED
+#define BIGREEN "\033[1;92m"      // GREEN
+#define BIYELLOW "\033[1;93m"     // YELLOW
+#define BIBLUE "\033[1;94m"       // BLUE
+#define BIPURPLE "\033[1;95m"     // PURPLE
+#define BICYAN "\033[1;96m"       // CYAN
+#define BIWHITE "\033[1;97m"      // WHITE
