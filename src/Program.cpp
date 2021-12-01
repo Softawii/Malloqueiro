5502b25490f1ff9289da2d138d7a98867549f042
FerroEduardo <47820549+FerroEduardo@users.noreply.github.com>
2021-11-30 23:57:17 -0300

    Teste de falha de compilação

--- src/Program.cpp
+++ src/Program.cpp
@@ -10,15 +10,15 @@
 
 int main(int argc, char const *argv[]) {
     
-    std::cout << "hello world" << std::endl;
-    // std::cout << sizeof(MalloqueiroLowLevel::s_block) << std::endl;
-    int * aa = (int *) Malloqueiro::malloc(4);
-    (*aa) = 10;
-    MalloqueiroGerency::memoryState();
-    std::cout << *(aa) << std::endl;
-    std::cout << (Malloqueiro::free(aa) ? "sucesso no free": "falha no free") << std::endl;
-    std::cout << *(aa) << std::endl;
-    MalloqueiroGerency::memoryState();
+    std::cout << "hello world" << std::endl
+    std::cout << UINT64_MAX << std::endl
+
+    // int * aa = (int *) Malloqueiro::malloc(-1);
+    // MalloqueiroGerency::memoryState();
+    // std::cout << *(aa) << std::endl;
+    // std::cout << (Malloqueiro::free(aa) ? "sucesso no free": "falha no free") << std::endl;
+
+    // MalloqueiroGerency::memoryState();
 
     return 0;
 }
