72b520b1d3e5c5f4ab98fd4283e0d91df55e11b2
FerroEduardo <47820549+ferroeduardo@users.noreply.github.com>
2021-12-06 02:24:17 -0300

    Preview dos benchmarks 3 e 4

--- src/Benchmark.cpp
+++ src/Benchmark.cpp
@@ -89,6 +89,56 @@ void benchmark_2() {
     std::cout << GREEN << "Benchmark 2: encerrado sem falhas. Tempo gasto: " << time_spent << "ms" << COLOR_RESET << std::endl;
 }
 
+/**
+ * @brief xxxxxxxxxxxxxxxxxxxx
+ * 
+ */
+void benchmark_3() {
+    Stopwatch stopwatch;
+    FREQUENCY(stopwatch);
+    START_STOPWATCH(stopwatch);
+    std::cout << "Benchmark 3: iniciando" << RED << std::endl;
+    #pragma omp parallel for
+    for (size_t j = 1024; j <= 1024000; j+= 1024) {
+        int *inteiros = (int *) Malloqueiro::malloc(sizeof(int) * j);
+        for (size_t i = 0; i < j; i++) {
+            inteiros[i] = 7;
+        }
+        for (size_t i = 0; i < j; i++) {
+            assert(inteiros[i] == 7);
+        }
+        Malloqueiro::free(inteiros);
+    }
+    STOP_STOPWATCH(stopwatch);
+    double time_spent = stopwatch.mElapsedTime;
+    std::cout << GREEN << "Benchmark 3: encerrado sem falhas. Tempo gasto: " << time_spent << "ms" << COLOR_RESET << std::endl;
+}
+
+/**
+ * @brief xxxxxxxxxxxxxx
+ * 
+ */
+void benchmark_4() {
+    Stopwatch stopwatch;
+    FREQUENCY(stopwatch);
+    START_STOPWATCH(stopwatch);
+    std::cout << "Benchmark 4: iniciando" << RED << std::endl;
+    #pragma omp parallel for
+    for (size_t j = 1024; j <= 1024000; j+= 1024) {
+        int *inteiros = (int *) malloc(sizeof(int) * j);
+        for (size_t i = 0; i < j; i++) {
+            inteiros[i] = 77;
+        }
+        for (size_t i = 0; i < j; i++) {
+            assert(inteiros[i] == 77);
+        }
+        free(inteiros);
+    }
+    STOP_STOPWATCH(stopwatch);
+    double time_spent = stopwatch.mElapsedTime;
+    std::cout << GREEN << "Benchmark 4: encerrado sem falhas. Tempo gasto: " << time_spent << "ms" << COLOR_RESET << std::endl;
+}
+
 int main(int argc, char const *argv[]) {
     Stopwatch stopwatch;
     if (argc < 2) {
@@ -101,6 +151,12 @@ int main(int argc, char const *argv[]) {
         benchmark_1(std::atoi(argv[2]), filepath);
     } else if (std::atoi(argv[1]) == 2) {
         benchmark_2();
+    } else if (std::atoi(argv[1]) == 3) {
+        /*
+        PASSAR RESULTADOS PARA ARQUIVO PARA PODER FAZER A MÉDIA
+        */
+        benchmark_3();
+        benchmark_4();
     } else {
         exit(-2);
     }
